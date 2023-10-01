b8c1bc370afc14630f944cd224f4cc646ddf29ed
Levente Révész <levente.revesz@gmail.com>
2023-10-01 13:02:02 +0200

    Send reply messages

--- plexbot.py
+++ plexbot.py
@@ -1,5 +1,6 @@
 import logging
 import asyncio
+import random
 from configparser import ConfigParser
 from pathlib import Path
 from telegram import Update
@@ -15,20 +16,41 @@ configpath = Path("~/.config/plexbot/config").expanduser()
 config = ConfigParser()
 config.read(configpath)
 
+def ok_reply() -> str:
+    return random.choice([
+        "Downloading ⬇️",
+        "Fetching it right now ⬇️",
+        "On it! ⬇️",
+        "Working on it ⬇️",
+        "Right away ⬇️",
+        "One moment ⌛",
+        "Just a moment ⏱️"])
+
+def success_reply() -> str:
+    return random.choice([
+        "Done ✅",
+        "Done 🎉",
+        "Finished ✅",
+        "Enjoy your video 🎉",
+        "Your video is ready 📺"])
 
 async def download_video(update: Update, context: CallbackContext) -> None:
     url = update.message.text
     videos = Path(config["Bot"]["videos-folder"])
 
     logging.info(f"Downloading video {url} ...")
-    
-    success = await download_with_yt_dlp(url, videos)
-    
-    if (success):
-        logging.info(f"Downloaded video {url}")
-    else:
-        logging.error(f"Download failed: {url}")
+    await context.bot.send_message(
+        update.message.chat_id,
+        ok_reply()
+    )
 
+    success = await download_with_yt_dlp(url, videos)
+
+    logging.info(f"Download {'complete' if success else 'failed'}: {url}")
+    await context.bot.send_message(
+        update.message.chat_id,
+        success_reply() if success else "Download failed ❌😭"
+    )
 
 async def download_with_yt_dlp(url, dest) -> bool:
     format_str = ''
